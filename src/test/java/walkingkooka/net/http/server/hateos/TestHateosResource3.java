9cfec719bf081be1f1de89156baabe6cfa3a7e20
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-09-06 22:31:40 +1000

    HateosContentTypeXmlNode removed

--- src/test/java/walkingkooka/net/http/server/hateos/TestHateosResource3.java
+++ src/test/java/walkingkooka/net/http/server/hateos/TestHateosResource3.java
@@ -78,25 +78,6 @@ public final class TestHateosResource3 extends FakeHateosResource<Optional<BigIn
                 TestHateosResource3.class);
     }
 
-    // toXmlNode........................................................................................................
-
-    @Override
-    public XmlNode toXmlNode() {
-        try {
-            final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            factory.setNamespaceAware(false);
-            factory.setValidating(false);
-            factory.setExpandEntityReferences(false);
-
-            final XmlDocument document = XmlNode.createDocument(factory.newDocumentBuilder());
-            return document.createElement(XmlName.element("test-hateos-resource-3"))
-                    .appendChild(document.createElement(XmlName.element("id"))
-                            .appendChild(document.createText(this.id.toString())));
-        } catch (final Exception cause) {
-            throw new Error(cause.getMessage(), cause);
-        }
-    }
-
     // Object...........................................................................................................
 
     @Override
