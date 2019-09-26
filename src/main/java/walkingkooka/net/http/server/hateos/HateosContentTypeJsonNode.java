f045557559e4b935578879c4b5f6f8498acc331a
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-09-26 13:19:48 +1000

    walkingkooka 26e95332def4e160a0a9643a2a669abbce7f5c4d 20190925
    - https://github.com/mP1/walkingkooka/pull/2182
    - text.printer.moved from io.printer

--- src/main/java/walkingkooka/net/http/server/hateos/HateosContentTypeJsonNode.java
+++ src/main/java/walkingkooka/net/http/server/hateos/HateosContentTypeJsonNode.java
@@ -17,11 +17,11 @@
 
 package walkingkooka.net.http.server.hateos;
 
-import walkingkooka.io.printer.IndentingPrinter;
-import walkingkooka.io.printer.IndentingPrinters;
-import walkingkooka.io.printer.Printers;
 import walkingkooka.net.header.MediaType;
 import walkingkooka.text.LineEnding;
+import walkingkooka.text.printer.IndentingPrinter;
+import walkingkooka.text.printer.IndentingPrinters;
+import walkingkooka.text.printer.Printers;
 import walkingkooka.tree.json.JsonNode;
 import walkingkooka.tree.json.marshall.FromJsonNodeContext;
 import walkingkooka.tree.json.marshall.ToJsonNodeContext;
