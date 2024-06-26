6b29e5b70d8b49f0dc5d6da2d11c9d59ff5239d3
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-06-26 11:09:59 +1000

    FakeHateosResourceHandlerContext

---
+++ src/main/java/walkingkooka/net/http/server/hateos/FakeHateosResourceHandlerContext.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.net.http.server.hateos;
+
+import walkingkooka.net.header.MediaType;
+import walkingkooka.tree.expression.ExpressionNumberKind;
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.marshall.FakeJsonNodeMarshallContext;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContextPreProcessor;
+
+import java.math.MathContext;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+
+public class FakeHateosResourceHandlerContext extends FakeJsonNodeMarshallContext implements HateosResourceHandlerContext {
+    @Override
+    public MediaType contentType() {
+        throw new UnsupportedOperationException();
+    }
+
+    // JsonNodeUnmarshallContext........................................................................................
+
+    @Override
+    public JsonNodeUnmarshallContext setPreProcessor(final JsonNodeUnmarshallContextPreProcessor processor) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> T unmarshall(final JsonNode json,
+                            final Class<T> type) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T extends Enum<T>> Set<T> unmarshallEnumSet(final JsonNode json,
+                                                        final Class<T> type,
+                                                        final Function<String, T> function) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> List<T> unmarshallList(final JsonNode json,
+                                      final Class<T> type) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> Set<T> unmarshallSet(final JsonNode json,
+                                    final Class<T> type) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <K, V> Map<K, V> unmarshallMap(final JsonNode json, Class<K> type,
+                                          final Class<V> type1) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> T unmarshallWithType(final JsonNode json) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> List<T> unmarshallWithTypeList(final JsonNode json) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> Set<T> unmarshallWithTypeSet(final JsonNode json) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <K, V> Map<K, V> unmarshallWithTypeMap(final JsonNode json) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MathContext mathContext() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ExpressionNumberKind expressionNumberKind() {
+        throw new UnsupportedOperationException();
+    }
+}
