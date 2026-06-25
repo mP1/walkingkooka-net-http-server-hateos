9aa840b8610afde43a2e8be6a4c997ae3874322d
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-06-25 12:29:39 +1000

    FakeHateosHttpHandler2

---
+++ src/main/java/walkingkooka/net/http/server/hateos/FakeHateosHttpHandler2.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.net.http.server.hateos;
+
+import walkingkooka.net.http.server.HttpRequest;
+import walkingkooka.net.http.server.HttpResponse;
+
+public class FakeHateosHttpHandler2<C extends HateosResourceHandlerContext> implements HateosHttpHandler2<C> {
+
+    public FakeHateosHttpHandler2() {
+        super();
+    }
+
+    @Override
+    public void handleConnect(final HttpRequest request,
+                              final HttpResponse response,
+                              final C context) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void handleDelete(final HttpRequest request,
+                             final HttpResponse response,
+                             final C context) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void handleGet(final HttpRequest request,
+                          final HttpResponse response,
+                          final C context) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void handleHead(final HttpRequest request,
+                           final HttpResponse response,
+                           final C context) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void handleOther(final HttpRequest request,
+                            final HttpResponse response,
+                            final C context) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void handlePatch(final HttpRequest request,
+                            final HttpResponse response,
+                            final C context) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void handlePost(final HttpRequest request,
+                           final HttpResponse response,
+                           final C context) {
+        throw new UnsupportedOperationException();
+    }
+}
